a8f62d1026dcd2c6073742962a17e877fe2369ae
MoriyaEster <moriyaeo100@gmail.com>
2023-06-01 17:14:43 +0300

    12 sivan

---
+++ Test.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "doctest.h"
+#include <stdexcept>
+#include <sstream>
+#include "iostream"
+
+#include "sources/MagicalContainer.hpp"
+
+using namespace ariel;
+
+TEST_SUITE("MagicalContainer Tests")
+{
+    TEST_CASE("Test size after adding elements")
+    {
+        MagicalContainer container;
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+        CHECK(container.size() == 0);
+
+    }
+
+}
